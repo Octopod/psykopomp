deb8a7dbf43a69645fe85ff7bdec09aa9150ab7d
Denise Nordlöf <denise.nordlof@gmail.com>
2017-07-01 23:52:48 +0200

    javascript connection to firebase works

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+//Start of ready-wrapper
+console.log( "Loading..." );
+
+    // Reference to database
+    var database = firebase.database();
+
+
+    // When clicking on the subscribe button
+    $( "#subscribe-btn" ).click(function() {
+    	console.log('schyrra rå');
+    	var input = document.getElementById("user-input").value;
+    	var newPostKey = firebase.database().ref().child('interest-signup').push().key;
+    	var updates = {};
+    	updates['/interest-signup/emails/' + newPostKey] = input;
+
+		firebase.database().ref().update(updates);
+
+    });
+
+
+    console.log( "main.js is ready!" );
+// End of ready-wrapper
+});
+
+
