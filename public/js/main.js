3982be975d45f8506042ecffa918b07f2992c03d
Octopod <denise.nordlof@gmail.com>
2017-09-27 14:32:30 +0200

    more stuff removed

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,61 @@
-$( document ).ready(function() {
-//Start of ready-wrapper
-console.log( "Loading..." );
+/*!
+ * Copyright 2017 Psykopomp scenkonst
+ */
 
-    // Reference to database
-    var database = firebase.database();
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
 
 
-    // When clicking on the subscribe button
-    $( "#subscribe-btn" ).click(function() {
-    	console.log('schyrra rå');
-    	var input = document.getElementById("user-input").value;
-    	var newPostKey = firebase.database().ref().child('interest-signup').push().key;
-    	var updates = {};
-    	updates['/interest-signup/emails/' + newPostKey] = input;
+  //#to-top button appears after scrolling
+  var fixed = false;
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+      if (!fixed) {
+        fixed = true;
+        $('#to-top').show("slow", function() {
+          $('#to-top').css({
+            position: 'fixed',
+            display: 'block'
+          });
+        });
+      }
+    } else {
+      if (fixed) {
+        fixed = false;
+        $('#to-top').hide("slow", function() {
+          $('#to-top').css({
+            display: 'none'
+          });
+        });
+      }
+    }
+  });
 
-		firebase.database().ref().update(updates);
-        loadPage(signup.html);
-
-    });
+})(jQuery); // End of use strict
 
 
-    console.log( "main.js is ready!" );
-// End of ready-wrapper
+// Button Intresseanmalan functionality
+var database = firebase.database();
+
+$('#subscribe-btn').click(function() {
+  var input = document.getElementById("user-input").value;
+  var newPostKey = firebase.database().ref().child('interest-signup').push().key;
+  var updates = {};
+  updates['/interest-signup/emails/' + newPostKey] = input;
+
+  firebase.database().ref().update(updates);
 });
-
-
